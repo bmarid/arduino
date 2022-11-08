05b58e15cd702134e96f01d02fbc018883ae25e2
tiffanalin <115715961+tiffanalin@users.noreply.github.com>
2022-11-08 20:04:23 +0100

    Update hbridge_millis.ino
    sense line 1

--- hbridge_millis/hbridge_millis.ino
+++ hbridge_millis/hbridge_millis.ino
@@ -11,16 +11,20 @@ int ENA_pin_2 = 10;
 int speed_EN_1 = 50;
 int speed_EN_2 = 58;
 
+<<<<<<< Updated upstream
+=======
+// light-dark sensor 1
 int pushButton = 7;
 
+
+>>>>>>> Stashed changes
 unsigned long level_time= 0;
+
 unsigned long time_go_right = 275; //time for rotation to the right, 90 degree
 unsigned long myMeter = 2500; //time to go 1 meter
 
 void setup() {
-  Serial.begin(9600); 
-  pinMode(pushButton, INPUT);
-     
+  Serial.begin(9600);   
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
   pinMode(motor2pin1, OUTPUT);
@@ -57,32 +61,39 @@ void go_left(int pos_1 = LOW, int pos_2 = HIGH) {
   go_right(pos_2, pos_1);
 }
 
-int counter = 0;
-
 void loop() {
   // time to connect a cabel to battery
-  delay(1000);  
-  
+  delay(2000);  
   analogWrite(ENA_pin_1, speed_EN_1);
   analogWrite(ENA_pin_2, speed_EN_2);
+<<<<<<< Updated upstream
+=======
   
   int buttonState = digitalRead(pushButton);
   Serial.println(buttonState);
   
+  //go forward with line 
   if(buttonState == 1){
-    counter = counter + 1;
-    Serial.println("counter = ");
-    Serial.println(counter);
-   }
-   
-   if(counter < 5) {
     Serial.println("Go forward");
     go_forward();
-   } else {
+   }
+  else {
     Serial.println("Go back");
-    go_right();
+    go_back();
     }
+>>>>>>> Stashed changes
 
+  unsigned long start_time = millis();
+ //1st
+  while (millis() - start_time < myMeter) {
+    Serial.println("1 FORWARD");
+    Serial.println(millis());
+    go_forward();
+  }
   
+<<<<<<< Updated upstream
   
 }
+=======
+}
+>>>>>>> Stashed changes
