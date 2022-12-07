c71bf88645b5c1b240617d5f1a6fbdd948cfbe56
tiffanalin <115715961+tiffanalin@users.noreply.github.com>
2022-12-07 09:58:44 +0100

    Update line_follower_w_PID.ino

--- line_follower_w_PID/line_follower_w_PID.ino
+++ line_follower_w_PID/line_follower_w_PID.ino
@@ -1,4 +1,4 @@
-/*Motors */
+// define motors
 int motor1pin1 = 2;
 int motor1pin2 = 3;
 
@@ -16,20 +16,30 @@ unsigned long time_go_right = 275;
 unsigned long myMeter = 2500; //1 meter
 unsigned long myMeter_first = 3000; //1 meter
 
-/*IR Sensors */
+// define IR sensors 
 int pushButton_L = 7; //L sensor
 int pushButton_R = 13; //R sensor 
 
-/* PID CONTROL */
-float Kp = 0;
-float Ki = 0;
-float Kd = 0;
-int P;
-int I; 
-int D; 
+// define constants for the PID controller
+const float kp = 1.0;
+const float ki = 0.1;
+const float kd = 0.01;
 
+// define variables to store the current and previous error values
+float error = 0.0;
+float prevError = 0.0;
+
+// define variables to store the current and previous time values
+unsigned long curTime = 0;
+unsigned long prevTime = 0;
+
+// define variables to store the current and previous output values
+float curOutput = 0.0;
+float prevOutput = 0.0;
+
+// initialize motors and sensors
 void setup() 
-{
+  {
   Serial.begin(9600);   
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
@@ -41,6 +51,7 @@ void setup()
   pinMode(pushButton_R, INPUT); //R sensor
 }
 
+// forward function 
 void go_forward(int pos_1 = HIGH, int pos_2 = LOW) { 
   //clockwise + clockwise, 
   digitalWrite(motor1pin1, pos_1);
@@ -51,11 +62,13 @@ void go_forward(int pos_1 = HIGH, int pos_2 = LOW) {
 //  delay(time_delay);
 }
 
+// back function
 void go_back(int pos_1 = HIGH, int pos_2 = LOW) {
   //anti-clockwise + anti-clockwise
   go_forward(pos_2, pos_1);
 }
 
+// right function 
 void go_right(int pos_1 = LOW, int pos_2 = HIGH) {
   analogWrite(ENA_pin_1, speed_new);
   analogWrite(ENA_pin_2, speed_new);
@@ -69,6 +82,7 @@ void go_right(int pos_1 = LOW, int pos_2 = HIGH) {
 //  delay(3000);
 }
 
+//stop function 
 void stop_func(int pos_1 = LOW, int pos_2 = HIGH) {
   digitalWrite(motor1pin1, pos_1); //added
   digitalWrite(motor1pin2, pos_1);
@@ -78,11 +92,13 @@ void stop_func(int pos_1 = LOW, int pos_2 = HIGH) {
   analogWrite(ENA_pin_2, 0);
 }
 
+//left function 
 void go_left(int pos_1 = LOW, int pos_2 = HIGH) {
   //anti-clockwise + clockwise
   go_right(pos_2, pos_1);
 }
 
+//travelling by millis 
 void go_forward_right(unsigned long level_time) {
   while (millis() < myMeter + level_time) {
     Serial.println("FORWARD");
@@ -94,67 +110,30 @@ void go_forward_right(unsigned long level_time) {
     go_right();
   }
 }
-/***************************************************************/
 
-void loop() 
-{
+void loop() {
+  // read the sensor values and calculate the error value
+  float sensorLeft = readSensor((pushButton_L);
+  float sensorRight = readSensor(pushButton_R);
+  error = sensorRight - sensorLeft;
 
-analogWrite(ENA_pin_1, speed_new);
-  analogWrite(ENA_pin_2, speed_new);
+  // calculate the time elapsed since the last iteration
+  curTime = millis();
+  float dt = curTime - prevTime;
 
-   int color = 0; // tracking white(0) , black(1) color
-   
-   if( (digitalRead(pushButton_L) == color) && (digitalRead(pushButton_R) == color)) {
-    go_forward();
-    Serial.println("FORWARD");
-   }
-   if( (digitalRead(pushButton_L) == color) && (digitalRead(pushButton_R) != color)) { 
-     go_left();
-      Serial.println("LEFT");
-   }  
-   if( (digitalRead(pushButton_L) != color) && (digitalRead(pushButton_R) == color)) { 
-     go_right();
-      Serial.println("RIGHT");
-   }
-   if( (digitalRead(pushButton_L) != color) && (digitalRead(pushButton_R) != color)) { 
-      go_back();
-       Serial.println("BACK");
-    }
-}
+  // calculate the proportional, integral, and derivative terms
+  float pTerm = kp * error;
+  float iTerm = ki * (error + prevError) * dt / 2.0;
+  float dTerm = kd * (error - prevError) / dt;
 
-/*****************************************************************************************
-PID control 
-Proportional = error
-Integral = I + error 
-Derivative = current error = last error 
-error = set point - position 
+  // calculate the output value
+  curOutput = pTerm + iTerm + dTerm;
 
-*/
-void PID_control() {
-  uint16_t position = qtr.readLineBlack(sensorValues); //read current position - I THINK WE NEED ENCODER
-  int error = 3500 - position; //3500 is the ideal position (the centre)
+  // adjust the robot's movement based on the output value
+  setMotors(curOutput, curOutput);
 
-  P = error;
-  I = I + error;
-  D = error - lastError;
-  lastError = error;
-  int motorspeed = P*Kp + I*Ki + D*Kd; //calculate the correction
-                                       //needed to be applied to the speed
-  
-  int motorspeeda = basespeeda + motorspeed;
-  int motorspeedb = basespeedb - motorspeed;
-  
-  if (motorspeeda > maxspeeda) {
-    motorspeeda = maxspeeda;
-  }
-  if (motorspeedb > maxspeedb) {
-    motorspeedb = maxspeedb;
-  }
-  if (motorspeeda < 0) {
-    motorspeeda = 0;
-  }
-  if (motorspeedb < 0) {
-    motorspeedb = 0;
-  } 
-  forward_brake(motorspeeda, motorspeedb);
+  // update the previous error, time, and output values
+  prevError = error;
+  prevTime = curTime;
+  prevOutput = curOutput;
 }
